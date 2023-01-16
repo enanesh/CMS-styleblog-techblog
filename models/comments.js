7420532d0652aae84d0a534ac2e0615283d342e3
enanesh <edna.ganesha.gr@gmail.com>
2023-01-15 16:58:00 -0800

    Adds model comments

--- models/comments.js
+++ models/comments.js
@@ -9,10 +9,40 @@ Comments.init(
             type: DataTypes.INTEGER,
             allowNull: false,
             autoIncrement: true,
-            primaryKey:true
+            primaryKey: true
             
         },
         
+        comment_text: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [10],
+            }
+        },
 
-    }
-)
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'Users',
+                key: 'id'
+            }
+        },
+        //add post id need to create post model firts
+     
+
+
+        
+
+    },
+    {
+        sequilize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comments'
+    },
+
+);
+
+module.exports = Comments;
