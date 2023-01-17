4864f28868ef8e26a1a11fa55eae4f08e3e7a487
enanesh <edna.ganesha.gr@gmail.com>
2023-01-16 19:41:26 -0800

    logout route set it is working but is not getting the info poperly it keeps showing an error on devtools

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -33,6 +33,7 @@ router.post('/login', async (req, res) => {
 });
 
 router.post('/logout', (req, res) => {
+    console.log("test logout")
     if (req.session.logged_in) {
         req.session.destroy(() => {
             res.status(204).end();
