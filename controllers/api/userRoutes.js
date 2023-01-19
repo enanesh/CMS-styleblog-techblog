d128bce9ae0f69b1c4a91bc617144c4bfa7701b4
enanesh <edna.ganesha.gr@gmail.com>
2023-01-18 21:32:49 -0800

    Fix bug from sign up request is working properly now

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,31 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+
+// CREATES SINGUP ROUTE 
+
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+
+
+
+
+
+//CREATES A LOGIN ROUTE
+
 router.post('/login', async (req, res) => {
     try {
         console.log("holaaaaa")
@@ -32,8 +57,13 @@ router.post('/login', async (req, res) => {
     }
 });
 
+
+
+// CREATES  LOGOUT ROUTE
+
 router.post('/logout', (req, res) => {
-    console.log("test logout")
+    console.log("logout");
+
     if (req.session.logged_in) {
         req.session.destroy(() => {
             res.status(204).end();
@@ -42,5 +72,6 @@ router.post('/logout', (req, res) => {
         res.status(404).end();
     }
 });
+
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -101,4 +101,16 @@ router.get('/login', (req, res) => {
 
 
 
+router.get('/signup', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+        console.log("user created ")
+        res.redirect('/');
+        return;
+    }
+
+    res.render('signup');
+});
+
+
 module.exports = router;
